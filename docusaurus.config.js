744fd1f0a9b9a360c06a855565059f77c47cf9e6
abdevlpr <abbouabdeldjebar@gmail.com>
2022-02-07 17:39:17 +0100

    fix: trying to add the second tab

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,124 +1,130 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (
-	module.exports = {
-		title: 'Lumia Stream Developer Documentation',
-		tagline: 'Connect your app to your smart lights seamlessly',
-		url: 'https://dev.lumiastream.com',
-		baseUrl: '/',
-		onBrokenLinks: 'throw',
-		onBrokenMarkdownLinks: 'warn',
-		favicon: 'img/favicon.ico',
-		organizationName: 'lumiastream', // Usually your GitHub org/user name.
-		projectName: 'lumiastream', // Usually your repo name.
+  module.exports = {
+    title: "Lumia Stream Developer Documentation",
+    tagline: "Connect your app to your smart lights seamlessly",
+    url: "https://dev.lumiastream.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "lumiastream", // Usually your GitHub org/user name.
+    projectName: "lumiastream", // Usually your repo name.
 
-		presets: [
-			[
-				'@docusaurus/preset-classic',
-				/** @type {import('@docusaurus/preset-classic').Options} */
-				({
-					api: {
-						sidebarPath: require.resolve('./sidebars.js'),
-						editUrl: 'https://github.com/lumiastream/Developer-Docs',
-					},
-					theme: {
-						customCss: require.resolve('./src/css/custom.css'),
-					},
-				}),
-			],
-		],
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          api: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            editUrl: "https://github.com/lumiastream/Developer-Docs",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
 
-		themeConfig:
-			/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-			({
-				navbar: {
-					title: 'Lumia Stream Developers',
-					logo: {
-						alt: 'Lumia Stream Logo',
-						src: 'img/logo.svg',
-					},
-					items: [
-						{
-							type: 'doc',
-							docId: 'intro',
-							position: 'left',
-							label: 'API Docs',
-						},
-						{
-							href: 'https://github.com/lumiastream/Developer-Docs',
-							label: 'GitHub',
-							position: 'right',
-						},
-					],
-				},
-				footer: {
-					style: 'dark',
-					links: [
-						{
-							title: 'API',
-							items: [
-								{
-									label: 'Intro',
-									to: '/docs/intro',
-								},
-								{
-									label: 'Rest',
-									to: '/docs/rest/clone-the-repo',
-								},
-								{
-									label: 'Websockets',
-									to: '/docs/websockets/clone-the-repo',
-								},
-							],
-						},
-						{
-							title: 'Community',
-							items: [
-								{
-									label: 'Discord',
-									href: 'https://discord.gg/R8rCaKb',
-								},
-								{
-									label: 'Twitter',
-									href: 'https://twitter.com/lumiastream',
-								},
-								{
-									label: 'Youtube',
-									href: 'https://youtube.com/lumiastream',
-								},
-								{
-									label: 'Instagram',
-									href: 'https://instagram.com/lumiastream',
-								},
-							],
-						},
-						{
-							title: 'More',
-							items: [
-								{
-									label: 'Main Website',
-									href: 'https://lumiastream.com',
-								},
-								{
-									label: 'Forum',
-									href: 'https://forum.lumiastream.com',
-								},
-								{
-									label: 'GitHub',
-									href: 'https://github.com/lumiastream/Developer-Docs',
-								},
-							],
-						},
-					],
-					copyright: `Copyright © ${new Date().getFullYear()} Lumia Stream, LLC. Built with lots of Lights.`,
-				},
-				prism: {
-					theme: lightCodeTheme,
-					darkTheme: darkCodeTheme,
-				},
-			}),
-	}
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        navbar: {
+          title: "Lumia Stream Developers",
+          logo: {
+            alt: "Lumia Stream Logo",
+            src: "img/logo.svg",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "intro",
+              position: "left",
+              label: "API Docs",
+            },
+            // {
+            //   type: "doc",
+            //   docId: "intro",
+            //   position: "left",
+            //   label: "Custom code",
+            // },
+            {
+              href: "https://github.com/lumiastream/Developer-Docs",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
+        },
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "API",
+              items: [
+                {
+                  label: "Intro",
+                  to: "/docs/intro",
+                },
+                {
+                  label: "Rest",
+                  to: "/docs/rest/clone-the-repo",
+                },
+                {
+                  label: "Websockets",
+                  to: "/docs/websockets/clone-the-repo",
+                },
+              ],
+            },
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "Discord",
+                  href: "https://discord.gg/R8rCaKb",
+                },
+                {
+                  label: "Twitter",
+                  href: "https://twitter.com/lumiastream",
+                },
+                {
+                  label: "Youtube",
+                  href: "https://youtube.com/lumiastream",
+                },
+                {
+                  label: "Instagram",
+                  href: "https://instagram.com/lumiastream",
+                },
+              ],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "Main Website",
+                  href: "https://lumiastream.com",
+                },
+                {
+                  label: "Forum",
+                  href: "https://forum.lumiastream.com",
+                },
+                {
+                  label: "GitHub",
+                  href: "https://github.com/lumiastream/Developer-Docs",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Lumia Stream, LLC. Built with lots of Lights.`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+        },
+      }),
+  }
 );
